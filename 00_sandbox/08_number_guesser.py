ec63b99ac28184cfebc7797f0245202c5ca38aed
bell.ashto <bell.ashto@awatapu.school.nz>
2022-02-25 14:10:06 +1300

    guessing numbers :)

--- 00_sandbox/08_number_guesser.py
+++ 00_sandbox/08_number_guesser.py
@@ -1,14 +1,18 @@
+from random import randint
+number = randint(1,10)
+
 lives = 3
 tries_attempted = 1
-print("Hello, I have a challenge for you! Can you guess the number I am currently thinking of?")
 
+print("Hello, I have a challenge for you! Can you guess the number I am currently thinking of?")
+print(number)
 while True:
     try:
         user_input = input("Now, guess the number! (Hint: It's between 1-10)")
-        if user_input == "6":
-            print("Wow! You guess the number! I know you could do it!")
+        if user_input == number:
+            print("Wow! You guessed the number! I know you could do it!")
             break
-        elif user_input != "6":
+        elif user_input != number:
             lives -= 1
             tries_attempted += 1
             print("Incorrect, try again! You have {} lives remaining".format(lives))

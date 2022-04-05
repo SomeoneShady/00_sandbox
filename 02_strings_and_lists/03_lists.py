574d5357d093f46b80118ab3b3b911c6b619950a
bell.ashto <bell.ashto@awatapu.school.nz>
2022-04-06 10:58:18 +1200

    pain

--- 02_strings_and_lists/03_lists.py
+++ 02_strings_and_lists/03_lists.py
@@ -1,1 +1,9 @@
-print("Hi friends putting this print here to test out github")
+scores = [1, 2, 3, 4, 5]
+names = ["Tim", "Tom", "Tam", "Tum", "John"]
+print(scores[2])
+print("{} got {} point.".format(names[0], scores[0]))
+print("{} got {} points.".format(names[1], scores[1]))
+print("{} got {} points.".format(names[2], scores[2]))
+print("{} got {} points.".format(names[3], scores[3]))
+print("{} got {} points.".format(names[4], scores[4]))
+

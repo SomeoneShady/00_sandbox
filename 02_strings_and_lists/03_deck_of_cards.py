f2c430d0ebdd431ce59b7eca34ef19e890125758
bell.ashto <bell.ashto@awatapu.school.nz>
2022-04-14 13:21:23 +1200

    not finished

--- 02_strings_and_lists/03_deck_of_cards.py
+++ 02_strings_and_lists/03_deck_of_cards.py
@@ -19,8 +19,31 @@ def card_deck():
                 deck.append(str(x) + item)
     return deck
 
+def player_number(num_of_players):
+    while True:
+        try:
+            response = int(input(num_of_players))
+            break
+        except ValueError:
+            print("That number of players isn't possible! Try again!")
+    return response
+
 
 print("Welcome! Have you had trouble dealing with shuffling a deck of cards?")
 print("Do you just want someone else to do it for you? Well step right up!")
 print("We have just the thing for you! The Card Drawer 2001 (V2)!")
-print("Just give us a moment and you'll have you cards in no time!")
+print("Just give us a moment and you'll have your cards in no time!")
+
+deck = card_deck()
+print(deck)
+print("This is your deck, now, let's shuffle it and hand out some cards!")
+shuffle(deck)
+
+
+num_of_players = player_number("How many players am I drawing cards for?")
+if num_of_players == 4:
+    print(deck[0:13]
+          deck[14:26]
+          deck[27:39]
+          deck[40:52])
+

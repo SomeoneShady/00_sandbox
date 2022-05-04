47b83ff0295ec831a2bb85e5fb6c7c175e41996f
bell.ashto <bell.ashto@awatapu.school.nz>
2022-05-05 11:57:21 +1200

    pick a card, any card

--- 02_strings_and_lists/03_deck_of_cards.py
+++ 02_strings_and_lists/03_deck_of_cards.py
@@ -42,8 +42,24 @@ shuffle(deck)
 
 num_of_players = player_number("How many players am I drawing cards for?")
 if num_of_players == 4:
-    print(deck[0:13]
-          deck[14:26]
-          deck[27:39]
-          deck[40:52])
+     player1 = deck[0:13]
+     player2 = deck[14:26]
+     player3 = deck[27:39]
+     player4 = deck[40:52]
+     print("""Player 1's hand will be {},"
+    "Player 2's hand will be {}"
+    "Player 3's hand will be {}"
+    "And finally, Player 4's hand will be {}, happy card gaming!""".format(player1, player2, player3, player4))
+elif num_of_players == 3:
+    player1 = deck[0:18]
+    player2 = deck[19:35]
+    player3 = deck[36:52]
+    print("""Player 1's hand will be {}
+    Player 2's hand will be {}
+    and Player 3's hand will be""".format(player1, player2, player3))
+elif num_of_players == 2:
+    player1 = deck[0:26]
+    player2 = deck[27:52]
+    print("""Player 1's hand will be {}
+    Player 2's hand will be {}""".format(player1, player2))
 

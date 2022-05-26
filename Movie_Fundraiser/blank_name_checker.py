d6f0735138899f7796b19e136bec21a516e48db4
bell.ashto <bell.ashto@awatapu.school.nz>
2022-05-26 13:29:59 +1200

    yay

--- Movie_Fundraiser/blank_name_checker.py
+++ Movie_Fundraiser/blank_name_checker.py
@@ -1,2 +1,18 @@
-name = input("What's you name?")
-print("Greetings {}, it is very nice to meet you!".format(name))
+# Function that constantly loops until the user provides a proper answer
+def anti_blank(question):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        if response.isdigit():
+            print("We cannot serve numbers, please gives us your real name.")
+        else:
+            while response != "":
+                return response
+            else:
+                print("Haha, very funny. Please gives us your name, we cannot serve the air.")
+
+# Greet the user and ask for their name
+name = anti_blank("Greetings! What is your name?")
+print("{}? What a wonderful name!".format(name))

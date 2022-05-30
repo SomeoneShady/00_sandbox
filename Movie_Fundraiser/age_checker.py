3fc7631d2a0e6c4ac331f503072664adfde89796
bell.ashto <bell.ashto@awatapu.school.nz>
2022-05-31 09:35:18 +1200

    not done yet

---
+++ Movie_Fundraiser/age_checker.py
@@ -0,0 +1,14 @@
+# Function that constantly loops until the user provides a proper answer
+def age_checker(question):
+    valid = False
+    while not valid:
+        try:
+            response = int(input(question))
+            break
+        except ValueError:
+            print("That's not a real age! Be honest!")
+    return response
+
+        # Ask the user how old they are
+age = age_checker("Now, can you tell me how old you are?")
+if age <= 12:
